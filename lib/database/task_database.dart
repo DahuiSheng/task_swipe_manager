190157d3586dc00065bac2905df2e61d04144647
DahuiSheng <71542476+DahuiSheng@users.noreply.github.com>
2023-04-01 23:42:46 +0900

    Add task_database.dart

---
+++ lib/database/task_database.dart
@@ -0,0 +1,82 @@
+import 'dart:async';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:task_swipe_manager/models/task.dart';
+
+class TaskDatabase {
+  static final TaskDatabase instance = TaskDatabase._init();
+
+  static Database? _database;
+
+  TaskDatabase._init();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+
+    _database = await _initDB('task.db');
+    return _database!;
+  }
+
+  Future<Database> _initDB(String filePath) async {
+    final dbPath = await getApplicationDocumentsDirectory();
+    final path = join(dbPath.path, filePath);
+
+    return await openDatabase(path, version: 1, onCreate: _createDB);
+  }
+
+  Future _createDB(Database db, int version) async {
+    final idType = 'INTEGER PRIMARY KEY AUTOINCREMENT';
+    final textType = 'TEXT NOT NULL';
+
+    await db.execute('''
+CREATE TABLE tasks (
+  id $idType,
+  title $textType,
+  description $textType,
+  status $textType
+)
+''');
+  }
+
+  Future<Task> create(Task task) async {
+    final db = await instance.database;
+    final id = await db.insert('tasks', task.toJson());
+    return task.copy(id: id);
+  }
+
+  Future<List<Task>> readAllTasks() async {
+    final db = await instance.database;
+    final orderBy = 'status';
+    final result = await db.query('tasks', orderBy: orderBy);
+
+    return result.map((json) => Task.fromJson(json)).toList();
+  }
+
+  Future<int> update(Task task) async {
+    final db = await instance.database;
+
+    return db.update(
+      'tasks',
+      task.toJson(),
+      where: 'id = ?',
+      whereArgs: [task.id],
+    );
+  }
+
+  Future<int> delete(int id) async {
+    final db = await instance.database;
+
+    return await db.delete(
+      'tasks',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+
+  Future close() async {
+    final db = await instance.database;
+
+    db.close();
+  }
+}
